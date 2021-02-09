991927c4f0ce1a072a58180f93c11f95ee8fef79
Taani Maama <taani1maama@live.com>
2021-02-08 18:34:00 -0800

    Working POST and PUT Routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,44 @@
-const { Workout } = require("../models")
-// const mongoose = require("mongoose");
+const { Workout } = require("../models");
 
 module.exports = app => {
-
+    // GET Route for pulling all of the workouts in DB.
     app.get("/api/workouts", async (req, res) => {
-        const data = await Workout.find()
+        const workouts = await Workout.find()
         try {
-            console.log(data)
-            res.json(data);
+            console.log(workouts)
+            res.json(workouts);
         } catch (error) {
             res.status(400).json(error);
         };
+    });
+
+    // POST Route for creating a workout.
+    app.post("/api/workouts", async ({ body }, res) => {
+        const newWorkout = await Workout.create(body)
+        try {
+            res.status(200).json(newWorkout)
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    });
+
+    // PUT Route for updating a workout.
+    app.put("/api/workouts/:id", async (req, res) => {
+        const updateWorkout = await Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            }
+        })
+        try {
+            res.status(200).json(updateWorkout)
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    });
+
+    // DELETE Route for deleting a workout.
+    app.delete("/api/workouts/:id", (req, res) => {
+
     });
 
 };
