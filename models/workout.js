84f2b5ea9ca372707726f8a066eb020f328f90e4
Taani Maama <taani1maama@live.com>
2021-02-11 11:45:53 -0800

    Finished the functionality of the stats page.

--- models/workout.js
+++ models/workout.js
@@ -1,44 +1,44 @@
 const mongoose = require("mongoose");
-mongoose.set('debug', true)
+mongoose.set("debug", true);
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
+const WorkoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
     },
     exercises: [
-        {
-            type: {
-                type: String,
-                required: [true, "This exercise needs a type."]
-            },
-            name: {
-                type: String,
-                required: [true, "This exercise needs a name."]
-            },
-            duration: Number,
-            weight: Number,
-            reps: Number,
-            sets: Number,
-            distance: Number
-        }
-    ]
-},
-{
+      {
+        type: {
+          type: String,
+          required: [true, "This exercise needs a type."],
+        },
+        name: {
+          type: String,
+          required: [true, "This exercise needs a name."],
+        },
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number,
+      },
+    ],
+  },
+  {
     toJSON: {
-        virtuals: true
-    }
-}
+      virtuals: true,
+    },
+  }
 );
 
-WorkoutSchema.virtual("totalDuration").get(function() {
-    return this.exercises.reduce((acc, exercise) => {
-        return acc + exercise.duration;
-    }, 0);
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((acc, exercise) => {
+    return acc + exercise.duration;
+  }, 0);
 });
-
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = Workout
+module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,44 +1,53 @@
 const { Workout } = require("../models");
 
-module.exports = app => {
-    // GET Route for pulling all of the workouts in DB.
-    app.get("/api/workouts", async (req, res) => {
-        const workouts = await Workout.find()
-        console.log(workouts)
-        try {
-            res.json(workouts);
-        } catch (error) {
-            res.status(400).json(error);
-        };
+module.exports = (app) => {
+  // GET Route for pulling all of the workouts in DB.
+  app.get("/api/workouts", async (req, res) => {
+    const workouts = await Workout.find();
+    console.log(workouts);
+    try {
+      res.json(workouts);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+
+  // POST Route for creating a workout.
+  app.post("/api/workouts", async ({ body }, res) => {
+    const newWorkout = await Workout.create(body);
+    try {
+      res.status(200).json(newWorkout);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+
+  // PUT Route for updating a workout.
+  app.put("/api/workouts/:id", async (req, res) => {
+    const updateWorkout = await Workout.findByIdAndUpdate(req.params.id, {
+      $push: {
+        exercises: req.body,
+      },
     });
+    try {
+      res.status(200).json(updateWorkout);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
 
-    // POST Route for creating a workout.
-    app.post("/api/workouts", async ({ body }, res) => {
-        const newWorkout = await Workout.create(body)
-        try {
-            res.status(200).json(newWorkout)
-        } catch (error) {
-            res.status(400).json(error)
-        }
+  app.get("/api/workouts/range", async (req, res) => {
+    const range = await Workout.find({
+      day: {
+        $gte: new Date().setDate(new Date().getDate() - 7),
+        $lte: Date.now(),
+      },
     });
-
-    // PUT Route for updating a workout.
-    app.put("/api/workouts/:id", async (req, res) => {
-        const updateWorkout = await Workout.findByIdAndUpdate(req.params.id, {
-            $push: {
-                exercises: req.body
-            }
-        })
-        try {
-            res.status(200).json(updateWorkout)
-        } catch (error) {
-            res.status(400).json(error)
-        }
-    });
-
-    // DELETE Route for deleting a workout.
-    app.delete("/api/workouts/:id", (req, res) => {
-
-    });
-
+    console.log(range);
+    try {
+      res.status(200).json(range);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
 };
